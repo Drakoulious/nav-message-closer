e7366280f905f98a506539c39283e6a027ba7ee5
dadahanov-mr <dadakhanov@gmail.com>
2021-09-02 09:31:11 +0300

    switch to 3.5 framework

--- NavMessageCloser/Form1.cs
+++ NavMessageCloser/Form1.cs
@@ -8,7 +8,7 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
+
 using System.Windows.Forms;
 using System.Configuration;
 using System.IO;
